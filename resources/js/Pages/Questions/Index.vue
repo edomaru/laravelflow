36d963067bc3ccfceadc643d98eda81721b63d1f
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-07-06 11:12:01 +0700

    Passing data to vue js component

--- resources/js/Pages/Questions/Index.vue
+++ resources/js/Pages/Questions/Index.vue
@@ -1,3 +1,16 @@
 <template>
     <h1>Welcome!</h1>
+
+    <div v-for="question in questions" :key="question.id">
+        {{ question.title }}
+    </div>
 </template>
+
+<script setup>
+defineProps({
+    questions: {
+        type: Array,
+        required: true
+    }
+})
+</script>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return inertia('Questions/Index');
+    return inertia('Questions/Index', [
+        'questions' => [
+            ['id' => 1, 'title' => 'Question 1'],
+            ['id' => 2, 'title' => 'Question 2'],
+        ]
+    ]);
 });
