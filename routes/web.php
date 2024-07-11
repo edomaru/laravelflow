51328a0d13beb286c1f53982697b80c657b91778
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-07-11 11:02:02 +0700

    Display questions

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,12 @@ use App\Models\Question;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return QuestionResource::collection(
+    $questions = QuestionResource::collection(
         Question::with('user')->latest()->paginate(15)
     );
-    
+
     return inertia('Questions/Index', [
-        'questions' => [
-            ['id' => 1, 'title' => 'Question 1'],
-            ['id' => 2, 'title' => 'Question 2'],
-        ]
+        'questions' => $questions
     ]);
 })->name('questions.index');
 
