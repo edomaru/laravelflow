77313fe155d6e107adcef524fce4af58bda89905
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-08-13 09:03:43 +0700

    Handle voting

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AcceptAnswerController;
 use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\BookmarkQuestionController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\VoteQuestionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [QuestionController::class, 'index'])->name('questions.index');
@@ -13,6 +14,7 @@ Route::resource('/questions', QuestionController::class)
     ->middleware('auth');
 Route::post('/questions/{question}/bookmark', [BookmarkQuestionController::class, 'store'])->name('questions.bookmark.store')->middleware('auth');
 Route::delete('/questions/{question}/bookmark', [BookmarkQuestionController::class, 'destroy'])->name('questions.bookmark.destroy')->middleware('auth');
+Route::post('/questions/{question}/vote', VoteQuestionController::class)->name('questions.vote')->middleware('auth');
 
 Route::resource('/questions.answers', AnswerController::class)->only(['store', 'update', 'destroy'])->middleware('auth');
 Route::post('/questions/answers/{answer}/accept', AcceptAnswerController::class)->name('questions.answers.accept')->middleware('auth');
